c32b02e3238ce50a4858ed9742c90b645a2d0253
Imran Hossain <imran.cse.ku@gmail.com>
2018-05-19 19:12:02 +0600

    comments

--- aspect_based_senti_analysis.py
+++ aspect_based_senti_analysis.py
@@ -13,53 +13,54 @@ trigrams_dic = {}
 aspect_opinion = {}
 rule = {}
 aspect_dic = dict()
-
-REGEX_REMOVE_PUNCTUATION = re.compile('[%s]' % re.escape(string.punctuation))
-
-not_list = ['no', 'nor', 'never', 'not', 'n\'t','don\'t',  'ain', 'aren', 'couldn', 'didn', 'doesn',
-            'hadn', 'hasn', 'haven', 'isn', 'mightn', 'mustn', 'needn', 'shouldn', 'wasn', 'weren', 'won\'t', 'wouldn']
+ 
+# defining class 
 class sentiAnalyzer(object):
     id = 0
     review = dict()
+    # reading data
     with open('nokia6610 (1).csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
             row[0].strip()
+            # assigning id to every sentence
             review[id] = row[0]
             id = id + 1
 
-    # print(review)
+    # checking but in the sentence
+    # if there is a but then split it
     review = {key: list(map(str, value.split('but'))) for key, value in review.items()}
     
+    # reading the aspect list and grouping it
     with open('Aspect.csv') as aspect_file:
         readAspect = csv.reader(aspect_file, delimiter=',')
         for aspect in readAspect:
             if aspect[0] != "Group Name":
                 aspect_dic[aspect[0]] = aspect[1]
 
-    # print(aspect_dic["Battery"])
-    
+    # insert comma for multiple aspect value in a group
     aspect_dic = {key: list(map(str, value.split(','))) for key, value in aspect_dic.items()}
     
     aspect_sentence_dic = dict()
-    # aspect_sentence_list_dic = {}
     
+    # checking for sentences which have the aspects
     for key,value in aspect_dic.items():
         aspect_sentence_list_dic = {}
         for word in value:
             for sentence_key, sentence_value in review.items():
+                #print(sentence_key)
                 for l_value in sentence_value:
                     if word.lower() in l_value:
                         aspect_sentence_list_dic[sentence_key] = l_value
-
         aspect_sentence_dic[key] =  aspect_sentence_list_dic
-        # print(aspect_sentence_dic["Speaker"])
-        # aspect_sentence_list = []
-    #print(aspect_sentence_dic["Sound Quality"])
+        
+        
+    # creating object of sentiment class
+    # this class belongs to the file sentiment.py
     s = sentiment.SentimentAnalysis(filename='SentiWordNet.txt',weighting='geometric')
     score_aspectwise_dic = {}
+    # calculating score for sentences
     for key, value in aspect_sentence_dic.items():
-        
         score_list = []
         for k, v in value.items():
             sentiments = s.score(v)
@@ -67,6 +68,7 @@ class sentiAnalyzer(object):
         score_aspectwise_dic[key] = score_list
     #print(score_aspectwise_dic)
     opinion_dic = {}
+    # calculation of positive and negative parcentage of every aspects score
     for key,value in score_aspectwise_dic.items():
         valence = []
         score = sum(value)/float(len(value))
@@ -83,5 +85,7 @@ class sentiAnalyzer(object):
         valence.append(pos_percent)
         valence.append(neg_percent)
         opinion_dic[key] = valence
-                
-    print(opinion_dic)
+    
+    # printing aspect wise polariy and positive and negative sentences percentages
+    for k, v in opinion_dic.items():
+        print(k+": ", round(v[0], 4), "pos:",round(v[1], 2), "%", "neg:",round(v[2], 2), "%")
